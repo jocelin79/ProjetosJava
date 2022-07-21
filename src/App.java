ecc79cda77c821a9c640ec771729f135f68c1b43
Jocelin Nunes <jocelinnunes@gmail.com>
2022-07-20 22:09:42 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "BIC Crisal";
+        c1.cor = "Azul";
+        c1.ponta = 0.5;
+        c1.status();
+    }
+}
