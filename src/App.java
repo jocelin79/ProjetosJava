9211200b4c600e7e8611758083a2fadf1e402d43
Jocelin Nunes <jocelinnunes@gmail.com>
2022-07-24 14:55:12 -0300

    up

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        
+        ControleRemoto c1 = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
     }
 }
--- src/ControleRemoto.java
+++ src/ControleRemoto.java
@@ -1,5 +1,110 @@
-public class ControleRemoto {
+public class ControleRemoto implements Controlador {
   private int volume;
   private boolean ligado;
-  "min 7:42 do vídeo";
+  private boolean tocando;
+
+  public ControleRemoto() {
+    this.volume = 50;
+    this.ligado = false;
+    this.tocando = false;
+  }
+
+  private int getVolume() {
+    return volume;
+  }
+
+  private boolean getLigado() {
+    return ligado;
+  }
+
+  private boolean getTocando() {
+    return tocando;
+  }
+
+  private void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  private void setLigado(boolean ligado) {
+    this.ligado = ligado;
+  }
+
+  private void setTocando(boolean tocando) {
+    this.tocando = tocando;
+  }
+
+  @Override
+  public void ligar() {
+    this.setLigado(true);
+  }
+
+  @Override
+  public void desligar() {
+    this.setLigado(false);
+  }
+
+  @Override
+  public void abrirMenu() {
+    System.out.println("Esta ligado?" + this.getLigado());
+    System.out.println("Esta tocando?" + this.getTocando());
+    System.out.print("Volume:" + this.getVolume());
+    for (int i = 0; i <= getVolume(); i += 10) {
+      System.out.print("|");
+    }
+  }
+
+  @Override
+  public void fecharMenu() {
+    System.out.println("Fechando Menu...");
+  }
+
+  @Override
+  public void maisVolume() {
+    if (this.getLigado()) {
+      this.setVolume(this.getVolume() + 5);
+    }
+  }
+
+  @Override
+  public void menosVolume() {
+    if (this.getLigado()) {
+      this.setVolume(this.getVolume() - 5);
+    }
+  }
+
+  @Override
+  public void ligarMudo() {
+    if (this.getLigado()) {
+      if (this.getVolume() > 0) {
+        this.setVolume(0);
+      }
+    }
+  }
+
+  @Override
+  public void desligarMudo() {
+    if (this.getLigado()) {
+      if (this.getVolume() == 0) {
+        this.setVolume(50);
+      }
+    }
+  }
+
+  @Override
+  public void play() {
+    if (this.getLigado()) {
+      if (!this.getTocando()) {
+        this.setTocando(true);
+      }
+    }
+  }
+
+  @Override
+  public void pause() {
+    if (this.getLigado()) {
+      if (this.getTocando()) {
+        this.setTocando(false);
+      }
+    }
+  }
 }
