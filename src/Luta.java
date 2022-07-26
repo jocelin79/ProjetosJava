dc1955f395e6150f7088b8846aa7f93717276d0e
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-26 14:54:19 -0300

    Create Luta class

---
+++ src/Luta.java
@@ -0,0 +1,25 @@
+public class Luta {
+  //Atributos
+  private Lutador desafiado;
+  private Lutador desafiado;
+  private int rounds;
+  private boolean aprovada;
+  
+  //Métodos Especiais
+  public Lutador getDesafiado() {
+  
+  };
+  
+  public void setDesafiado(Lutador desafiado) {
+    this.desafiado = desafiado;
+  };
+  
+  //Métodos Públicos
+  public void marcarLuta() {
+  
+  };
+  
+  public void lutar() {
+  
+  };
+}
