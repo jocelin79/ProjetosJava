0d850a44ca6e55d2b8f06e72955f89c578a56a9b
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-27 14:52:12 -0300

    up

--- src/Livro.java
+++ src/Livro.java
@@ -1,1 +1,16 @@
-
+public class Livro {
+  //Atributos
+  private String titulo;
+  private String autor;
+  private int totPaginas;
+  private int pagAtual;
+  private boolean aberto;
+  private Pessoa leitor;
+  
+  //Métodos Especiais
+  
+  //Métodos Público
+  public void detalhes () {
+    
+  };
+};
