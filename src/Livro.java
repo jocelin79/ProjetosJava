02ce61b726fb938db449e7d730fff04f6ebc5fe0
Jocelin Nunes <jocelinnunes@gmail.com>
2022-07-27 20:41:48 -0300

    up

--- src/Livro.java
+++ src/Livro.java
@@ -1,16 +1,98 @@
-public class Livro {
-  //Atributos
+public class Livro implements Publicacao {
+  // Atributos
   private String titulo;
   private String autor;
   private int totPaginas;
   private int pagAtual;
   private boolean aberto;
   private Pessoa leitor;
-  
-  //Métodos Especiais
-  
-  //Métodos Público
-  public void detalhes () {
-    
+
+  // Métodos Especiais
+  public Livro(String titulo, String autor, int totPaginas, Pessoa leitor) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.totPaginas = totPaginas;
+    this.leitor = leitor;
+    this.aberto = false;
+    this.pagAtual = 0;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getAutor() {
+    return autor;
+  }
+
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+
+  public int getTotPaginas() {
+    return totPaginas;
+  }
+
+  public void setTotPaginas(int totPaginas) {
+    this.totPaginas = totPaginas;
+  }
+
+  public int getPagAtual() {
+    return pagAtual;
+  }
+
+  public void setPagAtual(int pagAtual) {
+    this.pagAtual = pagAtual;
+  }
+
+  public boolean isAberto() {
+    return aberto;
+  }
+
+  public void setAberto(boolean aberto) {
+    this.aberto = aberto;
+  }
+
+  public Pessoa getLeitor() {
+    return leitor;
+  }
+
+  public void setLeitor(Pessoa leitor) {
+    this.leitor = leitor;
+  }
+
+  // Métodos Público
+  public String detalhes() {
+    return "Livro [aberto=" + aberto + ", autor=" + autor + ", leitor=" + leitor + ", pagAtual=" + pagAtual
+        + ", titulo=" + titulo + ", totPaginas=" + totPaginas + "]";
+  }
+
+  @Override
+  public void abrir() {
+    this.setAberto(true);
+  }
+
+  @Override
+  public void fechar() {
+    this.setAberto(false);
+  }
+
+  @Override
+  public void folhear(int pagina) {
+    this.setPagAtual(pagina);
+  }
+
+  @Override
+  public void avancarPag() {
+    this.setPagAtual(getPagAtual() + 1);
+  }
+
+  @Override
+  public void voltarPag() {
+    this.setPagAtual(getPagAtual() - 1);
   };
 };
