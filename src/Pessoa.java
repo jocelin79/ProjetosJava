b303d950f34789b9cd60fbabb2839cc966d11ca1
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-27 14:30:56 -0300

    get & set

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,5 +1,38 @@
 public class Pessoa {
   //Atributos
+  private String nome;
+  private int idade;
+  private String sexo;
+  
   //Métodos Especiais
+  public String getNome() {
+    return nome;
+  };
+  
+  public void setNome(String nome) {
+    this.nome = nome;
+  };
+  
+  public int getIdade() {
+   return idade;
+  };
+  
+  
+  public void setIdade(int idade) {
+    this.idade = idade;
+  };
+  
+  public String getSexo() {
+     return sexo;
+  };
+  
+  
+  public void setIdade(String sexo) {
+    this.sexo = sexo;
+  };
+  
   //Métodos Publicos
+  public void fazerAniver() {
+  
+  };
 };
