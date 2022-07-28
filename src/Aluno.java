ae3f2110ec877cc92d83309244147b05c89550e9
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-28 15:52:09 -0300

    up

---
+++ src/Aluno.java
@@ -0,0 +1,14 @@
+public class Aluno extends {
+  //Atributos
+  private int matr;
+  private String curso;
+  
+  //Métodos Especiais
+  
+  
+  //Métodos Publicos
+  
+  public void cancelarMatr() {
+    sout "Matrículo cancelada!";
+  }
+};
