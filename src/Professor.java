6959f472809acd08f381b432c8afb260182476c7
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-28 16:11:06 -0300

    up

---
+++ src/Professor.java
@@ -0,0 +1,13 @@
+public class Professor extends Pessoa{
+  //Atributos
+  private String especialidade;
+  private float salario;
+  
+  //Métodos Especiais
+  
+  
+  //Métodos Públicos
+  public void ReceberAum(float aum){
+    this.salario += aum;
+  };
+};
