234c27c799ba5978f84e0cd36f384c5343fd8fd1
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-29 15:08:13 -0300

    up

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
-       
+       Visitante v1 = new Visitante();
+        v1.setNome("Juvenal");
+        v1.setIdade(22);
+        v1.setSexo("M");
+        System.out.println(v1.toString());
     };
 }
