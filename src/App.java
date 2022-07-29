cbf528f1b8a286c423bd0848998fd4f3e8251b9c
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-29 15:18:03 -0300

    up

--- src/App.java
+++ src/App.java
@@ -5,5 +5,12 @@ public class App {
         v1.setIdade(22);
         v1.setSexo("M");
         System.out.println(v1.toString());
+        Aluno a1 = new Aluno();
+        a1.setNome("Cláudio");
+        a1.setMatricula(1111);
+        a1.setCurso("Informática");
+        a1.setIdade(16);
+        a1.setSexo("M");
+        a1.pagarMensalidade();
     };
 }
