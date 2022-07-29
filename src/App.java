6f744e7bb38ff9aa9818e91d780fb30b35378b47
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-29 14:39:09 -0300

    up

--- src/App.java
+++ src/App.java
@@ -1,21 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa();
-        Aluno p2 = new Aluno();
-        Professor p3 = new Professor();
-        Funcionario p4 = new Funcionario();
-
-        p1.setNome("Pedro");
-        p2.setNome("Maria");
-        p3.setNome("Cláudio");
-        p4.setNome("Fabiana");
-
-        p2.setCurso("Informática");
-        p3.setSalario(2500.75);
-        p4.setSetor("Estoque");
-
-        p1.ReceberAum(550.20);
-        p2.mudarTrabalho();
-        p3.
+       
     };
 }
