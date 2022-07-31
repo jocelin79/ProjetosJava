f76ca7b6f47c156b249eec185de34a554f8e8b0c
Jocelin Nunes <jocelinnunes@gmail.com>
2022-07-31 15:22:18 -0300

    up

--- src/App.java
+++ src/App.java
@@ -1,32 +1,8 @@
 public class App {
     public static void main(String[] args) {
-       Mamifero m1 = new Mamifero();
-       Reptil r1 = new Reptil();
-       Peixe p1 = new Peixe();
-       Ave a1 = new Ave();
+        Cachorro c1 = new Cachorro();
 
-       m1.setPeso(35.3f);
-       m1.getPeso();
-       m1.alimentar();
-       m1.locomover();
-       m1.emitirSom();
-
-       r1.setPeso(25.3f);
-       r1.getPeso();
-       r1.alimentar();
-       r1.locomover();
-       r1.emitirSom();
-
-       p1.setPeso(15.3f);
-       p1.getPeso();
-       p1.alimentar();
-       p1.locomover();
-       p1.emitirSom();
-
-       a1.setPeso(5.3f);
-       a1.getPeso();
-       a1.alimentar();
-       a1.locomover();
-       a1.emitirSom();
+        c1.reagir(8, 10);
+        c1.reagir(false);
     };
 }
