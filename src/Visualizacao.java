3579f3bf34acf7a90bc9f74201c9962ce0e014fc
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-08-01 17:14:40 -0300

    up

--- src/Visualizacao.java
+++ src/Visualizacao.java
@@ -1,1 +1,18 @@
-
+public class Visualizacao {
+  //Atributos
+  private Gafanhoto espectador;
+  private Video filme;
+  
+  //Métodos Especiais
+  public Visualizacao(Gafanhoto espectador, Video filme) {
+    this.espectador = espectador;
+    this.filme = filme;
+  };
+  
+  public Gafanhoto getEspectador() {
+    
+  };
+  
+  //Métodos Públicos
+  
+}
